899bfaa3b511f588824bc6cbc817fd72529665ca
Patryk Panek <patryyk221@gmail.com>
2023-01-14 18:08:19 +0100

    added to component Team mockData with react-data-grid

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,14 @@
-const Dashboard = () => { 
-    
-    return <div>dashboard</div>
- }
+import { Box } from '@mui/material';
+import Header from '../../components/Header';
 
- export default Dashboard;
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-beetwen" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
